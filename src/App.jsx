90da62fda28653b38691b23e096c0965f055d8aa
GGvibes <grherzog@gmail.com>
2024-12-18 17:14:20 -0600

    rendering FeatPup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import {puppyList} from './data.js'
-import './App.css'
-import {useState} from 'react';
+import { puppyList } from "./data.js";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log ("puppyList: ", puppyList)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
   return (
-    
-      <div>
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
-        }
-      </div>
-      
-  )
+    <div className="App">
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {
+        puppies.map((puppy) => {
+             return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
-export default App
+export default App;
